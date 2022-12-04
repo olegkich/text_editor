ed721c8e834d6f541386a5eeaf7e263ff402dfed
oleh <olegkich8@gmail.com>
2022-12-04 21:01:08 +0000

    Terminal Struct, fix some minor bugs

--- src/editor.rs
+++ src/editor.rs
@@ -13,8 +13,6 @@ pub struct Editor {
 impl Editor {
 
   pub fn run(&mut self) {    
-    let _stdout = stdout().into_raw_mode().unwrap();
-
     loop {
       if let Err(error) = self.refresh_screen() {
         die(&error);
@@ -31,7 +29,7 @@ impl Editor {
   }
   
   pub fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-    let pressed_key = read_key()?;
+    let pressed_key = Terminal::read_key()?;
     match pressed_key {
       Key::Ctrl('q') => self.should_quit = true,
       _ => (),
@@ -40,21 +38,23 @@ impl Editor {
   }
 
   pub fn refresh_screen(&self) -> Result<(), std::io::Error> {
-    // print!("\x1b[2J");
-    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    Terminal::cursor_hide();
+    Terminal::clear_screen();
+    Terminal::cursor_position(0, 0);
 
     if self.should_quit {
       println!("Goodbye.\r")
     } else {
       self.draw_rows();
-      print!("{}", termion::cursor::Goto(1, 1));
+      Terminal::cursor_position(0, 0);
     }
 
-    io::stdout().flush()
+    Terminal::cursor_show();
+    Terminal::flush()
   }
 
   fn draw_rows(&self) {
-    for _ in 0..self.terminal.size().height {
+    for _ in 0..self.terminal.size().height - 1 {
       println!("~\r");
     }
   }
@@ -67,15 +67,7 @@ impl Editor {
   }
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-  loop {
-    if let Some(key) = io::stdin().lock().keys().next() {
-      return key;
-    }
-  }
-}
-
-
 fn die(e: &std::io::Error) {
-	panic!("{:#?}", e);
+	Terminal::clear_screen();
+  panic!("{:#?}", e);
 }
