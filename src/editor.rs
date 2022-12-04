02324e8f80ea3b1745327d74396ff6190a434708
oleh <olegkich8@gmail.com>
2022-12-04 23:01:15 +0000

    move cursor

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,12 @@
 use crate::Terminal;
-use termion::event::Key;
+use termion::{event::Key, cursor::{Left, Right}};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const EDITOR_NAME: &str = "Sona editor"; 
 
 pub struct Position {
-  x: usize,
-  y: usize
+  pub x: usize,
+  pub y: usize
 }
 
 pub struct Editor {
@@ -42,8 +42,10 @@ impl Editor {
   
   pub fn process_keypress(&mut self) -> Result<(), std::io::Error> {
     let pressed_key = Terminal::read_key()?;
+    
     match pressed_key {
       Key::Ctrl('q') => self.should_quit = true,
+      Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(pressed_key),
       _ => (),
     }
     Ok(())
@@ -51,14 +53,14 @@ impl Editor {
 
   pub fn refresh_screen(&self) -> Result<(), std::io::Error> {
     Terminal::cursor_hide();
-    Terminal::clear_screen();
-    Terminal::cursor_position(0, 0);
+    Terminal::cursor_position(&Position{x: 0, y: 0});
 
     if self.should_quit {
+      Terminal::clear_screen();
       println!("Goodbye.\r")
     } else {
       self.draw_rows();
-      Terminal::cursor_position(0, 0);
+      Terminal::cursor_position(&self.cursor_position);
     }
 
     Terminal::cursor_show();
@@ -87,6 +89,27 @@ impl Editor {
     }
   }
 
+  fn move_cursor(&mut self, key: Key) {
+    let Position {mut x, mut y} = self.cursor_position;
+    let size = self.terminal.size();
+
+    let height = size.height.saturating_sub(1) as usize;
+    let width = size.width.saturating_sub(1) as usize;
+
+    match key {
+      Key::Down => if y < height {
+        y = y.saturating_add(1);
+      },
+      Key::Up => y = y.saturating_sub(1),
+      Key::Left => x = x.saturating_sub(1),
+      Key::Right => if x < width {
+        x = x.saturating_add(1);
+      },
+      _ => (),
+    }
+
+    self.cursor_position = Position {x, y};
+  }
   
 }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,9 @@
 use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::{IntoRawMode, RawTerminal}; 
+use termion::raw::{IntoRawMode, RawTerminal};
+
+use crate::editor::Position; 
 
 pub struct Size {
   pub width: u16,
@@ -30,9 +32,12 @@ impl Terminal {
     print!("{}", termion::clear::All);
   }
 
-  pub fn cursor_position(x: u16, y:u16) {
-    let x = x.saturating_add(1);
-    let y = y.saturating_add(1);
+  pub fn cursor_position(position: &Position) {
+    let Position{mut x,mut y} = position;
+    x = x.saturating_add(1);
+    y = y.saturating_add(1);
+    let x = x as u16;
+    let y = y as u16;
     print!("{}", termion::cursor::Goto(x, y));
   }
 
@@ -53,7 +58,7 @@ impl Terminal {
   }
 
   pub fn cursor_show() {
-    print!("{}", termion::cursor::Hide);
+    print!("{}", termion::cursor::Show);
   }
 
   pub fn clear_current_line() {
