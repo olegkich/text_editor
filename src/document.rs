26df29dddc669b90018010ce206a935eec78ab15
oleh <olegkich8@gmail.com>
2022-12-24 17:38:49 +0000

    scrol started

--- src/document.rs
+++ src/document.rs
@@ -4,24 +4,28 @@ use crate::Row;
 
 #[derive(Default)]
 pub struct Document {
-    rows: Vec<Row>,
+rows: Vec<Row>,
 }
 
 impl Document {
-    pub fn open(file_name: &str) -> Result<Self, std::io::Error> {
-        let contents = fs::read_to_string(file_name)?;
-        let mut rows = Vec::new();
-        for value in contents.lines() {
-            rows.push(Row::from(value));
-        }
+pub fn open(file_name: &str) -> Result<Self, std::io::Error> {
+  let contents = fs::read_to_string(file_name)?;
+  let mut rows = Vec::new();
+  for value in contents.lines() {
+      rows.push(Row::from(value));
+  }
 
-        Ok(Self { rows })
-    }
-    pub fn row(&self, index: usize) -> Option<&Row> {
-        self.rows.get(index)
-    }
+  Ok(Self { rows })
+}
+pub fn row(&self, index: usize) -> Option<&Row> {
+  self.rows.get(index)
+}
 
-    pub fn is_empty(&self) -> bool {
-        self.rows.is_empty()
-    }
+pub fn is_empty(&self) -> bool {
+  self.rows.is_empty()
+}
+
+pub fn len(&self) -> usize {
+  self.rows.len()
+}
 }
--- src/editor.rs
+++ src/editor.rs
@@ -9,135 +9,182 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Default)]
 pub struct Position {
-    pub x: usize,
-    pub y: usize,
+  pub x: usize,
+  pub y: usize,
 }
 
 pub struct Editor {
-    should_quit: bool,
-    terminal: Terminal,
-    cursor_position: Position,
-    document: Document,
+  should_quit: bool,
+  terminal: Terminal,
+  cursor_position: Position,
+  offset: Position,
+  document: Document,
 }
 
 impl Editor {
-    pub fn run(&mut self) {
-        loop {
-            if let Err(error) = self.refresh_screen() {
-                die(error);
-            }
-            if self.should_quit {
-                break;
-            }
-            if let Err(error) = self.process_keypress() {
-                die(error);
-            }
-        }
+  // main editor loop
+  pub fn run(&mut self) {
+    loop {
+      if let Err(error) = self.refresh_screen() {
+        die(error);
+      }
+      if self.should_quit {
+        break;
+      }
+      if let Err(error) = self.process_keypress() {
+        die(error);
+      }
     }
-    pub fn default() -> Self {
-        let args: Vec<String> = env::args().collect();
-        let document = if args.len() > 1 {
-            let file_name = &args[1];
-            Document::open(&file_name).unwrap_or_default()
-        } else {
-            Document::default()
-        };
+  }
 
-        Self {
-            should_quit: false,
-            terminal: Terminal::default().expect("Failed to initialize terminal"),
-            document,
-            cursor_position: Position::default(),
-        }
-    }
+  pub fn default() -> Self {
+    let args: Vec<String> = env::args().collect();
 
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        Terminal::cursor_hide();
-        Terminal::cursor_position(&Position::default());
-        if self.should_quit {
-            Terminal::clear_screen();
-            println!("Goodbye.\r");
-        } else {
-            self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+    let document = if args.len() > 1 {
+      let file_name = &args[1];
+      Document::open(&file_name).unwrap_or_default()
+    } else {
+        Document::default()
+      };
+
+    Self {
+      should_quit: false,
+      terminal: Terminal::default().expect("Failed to initialize terminal"),
+        offset: Position::default(),
+        document,
+        cursor_position: Position::default(),
+    }
+  }
+
+  fn refresh_screen(&self) -> Result<(), std::io::Error> {
+    Terminal::cursor_hide();
+    Terminal::cursor_position(&Position::default());
+    if self.should_quit {
+      Terminal::clear_screen();
+      println!("Goodbye.\r");
+    } else {
+      self.draw_rows();
+      Terminal::cursor_position(&Position {
+        x: self.cursor_position.x.saturating_sub(self.offset.x),
+        y: self.cursor_position.y.saturating_sub(self.offset.y)
+      });
+    }
+    Terminal::cursor_show();
+    Terminal::flush()
+  }
+
+  fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+    let pressed_key = Terminal::read_key()?;
+    match pressed_key {
+      Key::Ctrl('q') => self.should_quit = true,
+      Key::Up
+      | Key::Down
+      | Key::Left
+      | Key::Right
+      | Key::PageUp
+      | Key::PageDown
+      | Key::End
+      | Key::Home => self.move_cursor(pressed_key),
+      _ => (),
+    }
+    self.scroll();
+    Ok(())
+  }
+
+  pub fn scroll(&mut self) {
+    let Position {x, y} = self.cursor_position;
+
+    let width = self.terminal.size().width as usize;
+    let height = self.terminal.size().height as usize;
+
+    let mut offset = &mut self.offset;
+
+    // if cursor position is greater or equal to our height/width
+    // set the offset to height/width + 1
+    if y < offset.y {
+      offset.y = y;
+    } else if y >= offset.y.saturating_add(height) {
+      offset.y = y.saturating_sub(height).saturating_add(1);
+    }
+    if x < offset.x {
+      offset.x = x;
+    } else if x >= offset.x.saturating_add(width) {
+      offset.x = x.saturating_sub(width).saturating_add(1);
+    }
+  }
+
+  fn move_cursor(&mut self, key: Key) {
+    let Position { mut y, mut x } = self.cursor_position;
+
+    let height = self.document.len();
+    let width = if let Some(row) = self.document.row(y) {
+      row.len()
+    } else {
+      0
+    };
+    match key {
+      Key::Up => y = y.saturating_sub(1),
+      Key::Down => {
+        if y < height {
+          y = y.saturating_add(1);
         }
-        Terminal::cursor_show();
-        Terminal::flush()
-    }
-    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = Terminal::read_key()?;
-        match pressed_key {
-            Key::Ctrl('q') => self.should_quit = true,
-            Key::Up
-            | Key::Down
-            | Key::Left
-            | Key::Right
-            | Key::PageUp
-            | Key::PageDown
-            | Key::End
-            | Key::Home => self.move_cursor(pressed_key),
-            _ => (),
+      }
+      Key::Left => x = x.saturating_sub(1),
+      Key::Right => {
+        if x < width {
+            x = x.saturating_add(1);
         }
-        Ok(())
+      }
+      Key::PageUp => y = 0,
+      Key::PageDown => y = height,
+      Key::Home => x = 0,
+      Key::End => x = width,
+      _ => (),
+  }
+  self.cursor_position = Position { x, y }
+  }
+
+  fn draw_welcome_message(&self) {
+    let mut welcome_message = format!("Sona editor -- version {}", VERSION);
+
+    let width = self.terminal.size().width as usize;
+
+    // calculate placement of the message along x axis
+    let len = welcome_message.len();
+    let padding = width.saturating_sub(len) / 2;
+    let spaces = " ".repeat(padding.saturating_sub(1));
+
+    welcome_message = format!("~{}{}", spaces, welcome_message);
+    welcome_message.truncate(width);
+    println!("{}\r", welcome_message);
+  }
+
+  pub fn draw_row(&self, row: &Row) {
+    let width = self.terminal.size().width as usize;
+    
+    let start = self.offset.x;
+    let end = self.offset.x + width;
+
+    let row = row.render(start, end);
+    println!("{}\r", row)
+  }
+
+  fn draw_rows(&self) {
+    let height = self.terminal.size().height;
+    for terminal_row in 0..height - 1 {
+      Terminal::clear_current_line();
+      if let Some(row) = self.document.row(terminal_row as usize + self.offset.x) {
+        self.draw_row(row);
+      } else if self.document.is_empty() && terminal_row == height / 3 {
+        self.draw_welcome_message();
+      } else {
+        println!("~\r");
+      }
     }
-    fn move_cursor(&mut self, key: Key) {
-        let Position { mut y, mut x } = self.cursor_position;
-        let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
-        match key {
-            Key::Up => y = y.saturating_sub(1),
-            Key::Down => {
-                if y < height {
-                    y = y.saturating_add(1);
-                }
-            }
-            Key::Left => x = x.saturating_sub(1),
-            Key::Right => {
-                if x < width {
-                    x = x.saturating_add(1);
-                }
-            }
-            Key::PageUp => y = 0,
-            Key::PageDown => y = height,
-            Key::Home => x = 0,
-            Key::End => x = width,
-            _ => (),
-        }
-        self.cursor_position = Position { x, y }
-    }
-    fn draw_welcome_message(&self) {
-        let mut welcome_message = format!("Sona editor -- version {}", VERSION);
-        let width = self.terminal.size().width as usize;
-        let len = welcome_message.len();
-        let padding = width.saturating_sub(len) / 2;
-        let spaces = " ".repeat(padding.saturating_sub(1));
-        welcome_message = format!("~{}{}", spaces, welcome_message);
-        welcome_message.truncate(width);
-        println!("{}\r", welcome_message);
-    }
-    pub fn draw_row(&self, row: &Row) {
-        let start = 0;
-        let end = self.terminal.size().width as usize;
-        let row = row.render(start, end);
-        println!("{}\r", row)
-    }
-    fn draw_rows(&self) {
-        let height = self.terminal.size().height;
-        for terminal_row in 0..height - 1 {
-            Terminal::clear_current_line();
-            if let Some(row) = self.document.row(terminal_row as usize) {
-                self.draw_row(row);
-            } else if self.document.is_empty() && terminal_row == height / 3 {
-                self.draw_welcome_message();
-            } else {
-                println!("~\r");
-            }
-        }
-    }
+  }
 }
 
 fn die(e: std::io::Error) {
-    Terminal::clear_screen();
-    panic!("{}", e);
+  Terminal::clear_screen();
+  panic!("{}", e);
 }
